79308fa17fdea7db0f5f2c3143c7bc708d4c7f8e
Антон Молодяев <kotofruzik@KotoFruzik-2.local>
2026-03-06 13:59:01 +0300

    profile admin

--- lib/admin_home_page.dart
+++ lib/admin_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:autoschool_btgp/auth_service.dart';
+import 'package:autoschool_btgp/admin_profile_page.dart';
 
 class AdminHomePage extends StatefulWidget {
   @override
@@ -10,10 +11,10 @@ class AdminHomePage extends StatefulWidget {
 class _AdminHomePageState extends State<AdminHomePage> {
   int _selectedIndex = 0;
 
-  static const List<Widget> _pages = <Widget>[
-    Center(child: Text('Вы вошли как администратор', style: TextStyle(fontSize: 20))),
-    Center(child: Text('Тест', style: TextStyle(fontSize: 20))),
-    Center(child: Text('Профиль', style: TextStyle(fontSize: 20))),
+  static final List<Widget> _pages = <Widget>[
+    Center(child: Text('Вы вошли как администратор')),
+    Center(child: Text('Тест')),
+    AdminProfilePage(),
   ];
 
   void _onItemTapped(int index) {
@@ -31,30 +32,12 @@ class _AdminHomePageState extends State<AdminHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Администратор'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () => _signOut(context),
-          ),
-        ],
-      ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Главная',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.science),
-            label: 'Тест',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Профиль',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Главная'),
+          BottomNavigationBarItem(icon: Icon(Icons.science), label: 'Тест'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Профиль'),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
